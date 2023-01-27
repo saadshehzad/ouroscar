7b49eb3b3d46bd3e7cbe2e1486f6e012503f7c23
hamzali1 <zahh4500@gmail.com>
2023-01-27 15:34:42 -0800

    Add models for partner app

--- partner/models.py
+++ partner/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        abstract = True
+
+class Partner(TimeStampMixin):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    
+class PartnerAddress(TimeStampMixin):
+    partner = models.OneToOneField(Partner, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
