fc3eaada1519d09be8471d8a5b24213f6d243905
rabiyamustafa <rabiyamustafa8484@gmail.com>
2023-01-28 04:41:59 +0500

    Remove extra lines

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Product(models.Model):
     name = models.CharField(max_length=40)
@@ -29,8 +27,10 @@ class ProductOptions(models.Model):
 
 
 class ProductClass(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.ForeignKey(max_length=30)
     weight = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+
