819e6d80cdc742e10cadbda36276fdea2ef6f45b
hamzali1 <zahh4500@gmail.com>
2023-01-28 10:15:55 -0800

    Create partne, partner, mgration

--- partner/models.py
+++ partner/models.py
@@ -3,17 +3,25 @@ from django.contrib.auth.models import User
 
 
 class TimeStampMixin(models.Model):
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, null=True)
     
     class Meta:
         abstract = True
 
-class Partner(TimeStampMixin):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Partners(TimeStampMixin):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     
+    class Meta:
+        verbose_name = verbose_name_plural = "Partners"
+        
+    def __str__(self):
+        return f"{self.user.first_name}"
     
 class PartnerAddress(TimeStampMixin):
-    partner = models.OneToOneField(Partner, on_delete=models.CASCADE)
+    partner = models.OneToOneField(Partners, on_delete=models.CASCADE)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.partner.user.first_name, self.city, self.country}"
