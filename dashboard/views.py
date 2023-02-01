9e59224c08ae571051f0d02b7790704db4d49ed4
hamzali1 <zahh4500@gmail.com>
2023-02-01 14:15:11 -0800

    startapp named as dashboard, with dashboard view

---
+++ dashboard/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from catalogue.models import *
+
+
+
+def dashboard(request):
+    product = Product.objects.all().count()
+    category = Category.objects.all().count()
+    context = {"product":product, "category":category}
+    return render(request, "catalogue/dashboard.html", context)
