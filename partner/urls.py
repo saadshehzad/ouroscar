8c726ad0635f3ac310ff80040ef837987f91cc6b
hamzali1 <zahh4500@gmail.com>
2023-02-08 10:48:32 -0800

    rename urls of partneraddress crud

--- partner/urls.py
+++ partner/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("list/", partneraddress_list),
-    path("create/", creat_partneraddress),
-    path("detail/<int:id>", partneraddress_detail),
-    path("update/<int:id>", partneraddress_update),
-    path("delete/<int:id>", delete_partneraddress),
+    path("address/list/", partneraddress_list, name="list"),
+    path("address/create/", creat_partneraddress),
+    path("address/detail/<int:id>", partneraddress_detail),
+    path("address/update/<int:id>", partneraddress_update),
+    path("address/delete/<int:id>", delete_partneraddress),
+    
+    # partner
+    
 ]
