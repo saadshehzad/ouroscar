4e4a341efddeb902c3be5e78dbdd74778567a840
rabiyamustafa <rabiyamustafa8484@gmail.com>
2023-02-13 03:13:42 +0500

    Add partner profile view

--- partner/urls.py
+++ partner/urls.py
@@ -3,12 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("address/list/", partneraddress_list, name="list"),
-    path("address/create/", creat_partneraddress),
-    path("address/detail/<int:id>", partneraddress_detail),
-    path("address/update/<int:id>", partneraddress_update),
-    path("address/delete/<int:id>", delete_partneraddress),
-    
-    # partner
     path("create/", create_partner),
+    path("profile", partner_profile, name="profile")
 ]
