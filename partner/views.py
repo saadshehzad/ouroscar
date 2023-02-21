e2711a7ba8681f150e2661373bbcbad1820ba95f
hamzali1 <zahh4500@gmail.com>
2023-02-21 12:14:51 -0800

    Add partner login view

--- partner/views.py
+++ partner/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponse, redirect, render
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import login, logout, authenticate
 from .forms import *
 from .models import *
 
@@ -28,6 +29,24 @@ def create_partner(request):
     return render(request, "partner/create_partner.html")
 
 
+def partner_login(request):
+    form = PartnerLoginForm()
+    if request.method == "POST":
+        form = PartnerLoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect("partner_profile")
+            else:
+                return HttpResponse("Invalide credentials.")
+    context = {"form": form}
+    return render(request, "registration/login.html", context)
+    
+
+
 def partner_profile(request):
     partner = Partners.objects.get(user=request.user)
     context = {
@@ -39,3 +58,27 @@ def partner_profile(request):
         "phone": partner.phone,
     }
     return render(request, "partner/partner_profile.html", context)
+
+
+
+def user_register_view(request):
+    if request.method == "POST":
+        try:
+            first_name = request.POST.get("first_name")
+            last_name = request.POST.get("last_name")
+            username = request.POST.get("username")
+            email = request.POST.get("email")
+            password = make_password(request.POST.get("password"))
+            all_users = User.objects.all()
+            for user in all_users:
+                if email == user.email:
+                    return HttpResponse("User with this email already exists.")
+            user = User.objects.create(first_name=first_name,
+                last_name=last_name, username=username, email=email,
+                password=password)
+            # Partners.objects.create(user=user,) We do not need that while creating normal user
+            return redirect("login")
+        except:
+            return HttpResponse("Cannot Create partner")
+    return render(request, "partner/create_partner.html")
+
