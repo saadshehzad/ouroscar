47c4d8cacf174244a0140d0a7dadc7c57c5fa9d7
hamzali1 <zahh4500@gmail.com>
2023-02-21 12:13:06 -0800

    Register Custom user model

--- partner/admin.py
+++ partner/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Partners
+from .models import Partners, CustomUser
 
+admin.site.register(CustomUser)
 
 @admin.register(Partners)
 class PartnersAdmin(admin.ModelAdmin):
