372369dbfd27edda7b41ef9e71091737903202d0
hamzali1 <zahh4500@gmail.com>
2023-02-21 12:13:34 -0800

    Add forms for custom user and partner login

--- partner/forms.py
+++ partner/forms.py
@@ -2,5 +2,11 @@ from django import forms
 from .models import *
 
 
-
-
+class CustomUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields ="__all__"
+        
+class PartnerLoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput)
